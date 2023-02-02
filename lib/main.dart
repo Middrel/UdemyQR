85445825f6a5682f0ada253831ed38dcc8e20016
Middrel <middrel@gmail.com>
2023-02-01 22:10:09 -0600

    QR Scanner

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/home_page.dart';
+import 'package:qr_reader/pages/map_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => UiProvider())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'QR Reader',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => const HomePage(),
+          'mapa': (_) => const MapPage()
+        },
+        theme: ThemeData(
+            primaryColor: Colors.deepPurple,
+            floatingActionButtonTheme: FloatingActionButtonThemeData(
+                backgroundColor: Colors.deepPurple)),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/addresses_page.dart';
+import 'package:qr_reader/pages/maps_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
+import 'package:qr_reader/widgets/custom_navbar.dart';
+import 'package:qr_reader/widgets/scan_buttonbar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Historial'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.delete_forever))
+        ],
+      ),
+      body: MapsPage(),
+      bottomNavigationBar: CustomNavbar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Obtener el selected menu opt
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    // Cambiar para mostrar la página respectiva
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    switch (currentIndex) {
+      case 0:
+        return MapsPage();
+      case 1:
+        return AddressesPage();
+      default:
+        return MapsPage();
+    }
+  }
+}
